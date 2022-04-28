cc27af0fa58a22433470d471e45d81c1c10f9f6a
Use your GitHub damianhunt <Enter your dhuntsa@gmail.com>
2022-04-28 21:03:38 +0200

    commit

---
+++ week02prepare01.js
@@ -0,0 +1,21 @@
+``` 1.Add two numbers
+    2.Multiply two numbers
+    3.Subtract one from a number
+    4.concatinate two strings together.
+    5.assign a value to a variable
+    6.increment the value in a variable by 3
+    7.compare two values to see if they are the same
+    8.check to see if one number is less than another number
+    9.Check to see if two values are NOT equal
+    10.check to see if a value is less than 10 and greater than 0```
+x = 2+2
+x = 2*2
+x = 2-1
+x = "hello" + "world"
+x = 5
+x = 6++; 6++; 6++
+x = 5; y = 5; x == y
+x = 5; y = 4; x < y
+x = 5; y = 4; x != y
+x = 5; x < 10 ; x > 0
+
