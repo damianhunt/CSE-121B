2fd2afafcc074d66f62a0b01fb2f52303c299c54
damianhunt <dhuntsa@gmail.com>
2022-05-02 21:19:00 +0200

    commit

--- CSE-121B/week03/scripts/task3.js
+++ CSE-121B/week03/scripts/task3.js
@@ -6,6 +6,7 @@
 function add(number1, number2) {
 // Step 2: In the function, return the sum of the parameters number1 and number2
     return number1 + number2;
+
 }
 // Step 3: Step 3: Using function declaration, define another function named addNumbers that gets the values of two HTML form controls with IDs of addend1 and addend2. Pass them to the add function
 function addnumbers(){
@@ -15,12 +16,14 @@ function addnumbers(){
 
 // Step 4: Assign the return value to an HTML form element with an ID of sum
     document.querySelector("#sum").value = result;
+
 }
 // Step 5: Add a "click" event listener to the HTML button with an ID of addNumbers that calls the addNumbers function
 document.querySelector("#addNumbers").addEventListener("click", addnumbers);
 // Step 6: Using function expressions, repeat Steps 1-5 with new functions named subtract and subtractNumbers and HTML form controls with IDs of minuend, subtrahend, difference and subtractNumbers
 const subtract = function (number1, number2) {
     return number1 - number2;
+
 }
 
 const subtractNumbers = function (){
@@ -28,6 +31,7 @@ const subtractNumbers = function (){
     let subtrahend = parseInt(document.querySelector("#subtrahend").value);
     let result = subtract(minuend - subtrahend);
     document.querySelector("#difference").value = result;
+
 }
 
 document.querySelector("#subtractNumbers").addEventListener("click", subtractNumbers);
@@ -39,18 +43,21 @@ const multiplyNumbers = () => {
     let factor2 = parseInt(document.querySelector("#factor2").value);
     let result = multiply(factor1 * factor2);
     document.querySelector("#product").value = result;
+
 }
 document.querySelector("#multiplyNumbers").addEventListener("click", multiplyNumbers);
 
 // Step 8: Using any of the three function declaration types, repeat Steps 1-5 with new functions named divide and divideNumbers and HTML form controls with IDs of dividend, divisor, quotient and divideNumbers
 const divide = function(number1, number2) {
     return number1 / number2;
+
 }
 const divideNumbers = function(dividend, divisor) {
     let dividend = parseInt(document.querySelector("#dividend").value);
     let divisor = parseInt(document.querySelector("#divisor").value);
     let result = divide(dividend / divisor)
     document.querySelector("#quotient").value = result;
+    
 }
 document.querySelector("#divideNumbers").addEventListener("click", divideNumbers);
 
